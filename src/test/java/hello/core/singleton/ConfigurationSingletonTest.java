7b19dcf3cc0218a32575a8482b5f0d0ff6978751
KangHo-dev <tiger452003@gmail.com>
2022-11-30 02:54:20 +0900

    @Configuration과 바이트코드 조작의 마법

--- src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
+++ src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -33,4 +33,13 @@ public class ConfigurationSingletonTest {
         
         
     }
+
+    // CGLIB
+    @Test
+    void configurationDeep() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        AppConfig bean = ac.getBean(AppConfig.class);
+
+        System.out.println("bean = " + bean.getClass());
+    }
 }
