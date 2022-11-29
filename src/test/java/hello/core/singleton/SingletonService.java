1a63b996e5b9bc5a1c835008bd7d6021cf87dff6
KangHo-dev <tiger452003@gmail.com>
2022-11-29 23:58:38 +0900

    싱글톤 패턴/컨테이너

--- src/test/java/hello/core/singleton/SingletonService.java
+++ src/test/java/hello/core/singleton/SingletonService.java
@@ -9,8 +9,7 @@ public class SingletonService {
     }
 
     private SingletonService() {}
-
     public void logic() {
-        System.out.println("싱글톤 객체 로직 호출");
+        System.out.println(" 싱글톤 객체 로직 호출 ");
     }
 }
