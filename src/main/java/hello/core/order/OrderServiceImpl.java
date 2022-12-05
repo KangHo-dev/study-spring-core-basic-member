94f6f3fcab8b7e777a2246ac3a66c8aa647e516e
KangHo-dev <tiger452003@gmail.com>
2022-12-05 23:14:19 +0900

    롬복과 최신 트랜드

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -4,20 +4,20 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
+
+    //final (필수값) 을 생성자로 만들어 준다 -> RequiredArgsConstructor
+    //lombok 제공 , 의존관계 주입이 매우 쉬워진다.  (ctrl+f12 로 확인 가능)
+    // compile 시점에 생성자 코드를 자동으로 생성
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
-
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
