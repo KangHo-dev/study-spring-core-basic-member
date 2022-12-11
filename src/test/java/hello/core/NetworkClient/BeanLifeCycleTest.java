ae4ce1938bc717d6ae1e91ba51d076802b77a1c1
KangHo-dev <tiger452003@gmail.com>
2022-12-12 00:06:49 +0900

    빈 생명주기 콜백 시작

--- src/test/java/hello/core/NetworkClient/BeanLifeCycleTest.java
+++ src/test/java/hello/core/NetworkClient/BeanLifeCycleTest.java
@@ -1,6 +1,9 @@
 package hello.core.NetworkClient;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,7 +11,9 @@ public class BeanLifeCycleTest {
 
     @Test
     public void lifeCycleTest() {
-
+        ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
+        NetworkClient client = ac.getBean(NetworkClient.class);
+        ac.close();
     }
 
     @Configuration
