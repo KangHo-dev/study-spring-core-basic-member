67d452e224db373df0fae479b55dbaffbbea9bbd
KangHo-dev <tiger452003@gmail.com>
2022-12-11 23:59:49 +0900

    빈 생명주기 콜백 시작

---
+++ src/test/java/hello/core/NetworkClient/BeanLifeCycleTest.java
@@ -0,0 +1,24 @@
+package hello.core.NetworkClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class BeanLifeCycleTest {
+
+    @Test
+    public void lifeCycleTest() {
+
+    }
+
+    @Configuration
+    static class LifeCycleConfig {
+
+        @Bean
+        public NetworkClient networkClient() {
+            NetworkClient networkClient = new NetworkClient();
+            networkClient.setUrl("http://hello-spring.dev");
+            return networkClient;
+        }
+    }
+}
