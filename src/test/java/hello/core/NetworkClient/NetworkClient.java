5f85cf325dccb593114b389dcfb05e848c6a71bc
KangHo-dev <tiger452003@gmail.com>
2022-12-12 23:41:50 +0900

    인터페이스 InitializingBean, DisposableBean

--- src/test/java/hello/core/NetworkClient/NetworkClient.java
+++ src/test/java/hello/core/NetworkClient/NetworkClient.java
@@ -1,12 +1,22 @@
 package hello.core.NetworkClient;
 
-public class NetworkClient {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+/**
+ * InitializingBean : 의존관계 주입 후에 호출
+ * DisposableBean : 빈이 종료될 때 호출
+ *
+ * -> 인터페이스를 사용하는 초기화,종료 방법은 스프링 초기(2003년)에 나온 방법 / 지금은 더 나은 방법들이 사용되기 때문에
+ * 거의 쓰이지 않음
+ */
+
+public class NetworkClient implements InitializingBean , DisposableBean {
     private String url;
 
     public NetworkClient() {
         System.out.println("생성자 호출, url = " + url);
-        connect();
-        call("초기화 연결 메시지");
+
     }
 
     public void setUrl(String url) {
@@ -25,5 +35,17 @@ public class NetworkClient {
     //서비스 종료시 호출
     public void disconnect() {
         System.out.println("close: " + url);
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("NetworkClient.destroy");
+        disconnect();
     }
 }
